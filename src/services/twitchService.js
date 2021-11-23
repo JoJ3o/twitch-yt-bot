b9c1622b0ffd51532af5dd7fa3897ecf167db34c
JoJ3o <42371079+JoJ3o@users.noreply.github.com>
2021-11-23 16:19:29 +0100

    Format and announce twitch raffle

--- src/services/twitchService.js
+++ src/services/twitchService.js
@@ -1,6 +1,7 @@
 const tmi = require('tmi.js');
 
 let raffleUsersEntered = [];
+let client;
 
 const twitchService = {};
 
@@ -8,7 +9,7 @@ twitchService.startRaffle = async(data) => {
 	console.log('Start Twitch Raffle');
 	console.log(data);
 
-	const client = new tmi.Client({
+	client = new tmi.Client({
 		options: {
 			debug: true
 		},
@@ -16,19 +17,24 @@ twitchService.startRaffle = async(data) => {
 			username: 'jobodev',
 			password: 'oauth:703yewp5pgp66q3n7avb6ttrdua138'
 		},
-		channels: [ 'joj3o' , 'osjesleben' ]
+		channels: [ 'joj3o', 'osjesleben' ]
 	});
 
-	client.connect();
+	client.connect().then(() => {
+		client.say('#osjesleben', 'Raffle started! Type !join to enter')
+	});
+
 
 	client.on('message', (channel, tags, message, self) => {
 		// Ignore echoed messages.
 		if(self) return;
-
+		console.log(channel);
 		filterMessage(channel, tags, message, data.enterMessage);
 	});
 
 	setTimeout(function() {
+		console.log(...raffleUsersEntered),
+		client.say('#osjesleben', 'The winners of the raffle are: ' + raffleUsersEntered.join(", ")),
 		client.disconnect(),
 		endRaffle()
 	}, (data.duration * (60/2) * 1000));
